09c0b69a400e203e6d4e92f28405e648dc693042
Prangwalai <waepwl4224@gmail.com>
2025-05-15 02:07:34 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:80/FG-Stock';
+    public string $baseURL = 'http://128.199.225.122/FG-Stock';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
